91dcaf9af4096b087b1c3c084e437f0902b659db
GabrielMatiasn10 <gm4037003@gmail.com>
2023-10-02 14:31:12 -0300

    fim

--- src/assets/Footer.jsx
+++ src/assets/Footer.jsx
@@ -1,9 +1,17 @@
+import { BsInstagram } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
+
 
 function Footer() {
   return (
+    <div>
     <footer>
-      <p>© 2021</p>
+    <a href="https://www.instagram.com/gabrielmatiasn10/" target="_blank"> <BsInstagram/> </a>
+    <a href="https://api.whatsapp.com/send?phone=551195003-9320&text=Ol%C3%A1%20Gabriel%20Matias%2C%20tudo%20bem%3F%20" target="_blank"> <BsWhatsapp/> </a>
     </footer>
+    <h5>@</h5>
+    </div>
+    
   );
 }
 
