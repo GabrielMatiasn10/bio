77f5d3fb37bfb0ad6d808a866cd5434df31086fe
GabrielMatiasn10 <gm4037003@gmail.com>
2023-10-02 13:38:42 -0300

    Comeco

---
+++ src/assets/Main.jsx
@@ -0,0 +1,19 @@
+
+
+function Main() {
+  return (
+    <div>
+        <body>
+    <img src="https://avatars.githubusercontent.com/u/91384364?v=4" alt="Gabriel Matias"/>
+    <p>Gabriel Matias</p>
+
+    <main>
+        <button> <a href="https://github.com/GabrielMatiasn10" target="_blank">  Veja Meu Portifolio </a></button>
+        <button> <a href="https://portifolio-lyart-zeta.vercel.app/" target="_blank">  Meu Github </a></button>
+    </main>
+    </body>
+    </div>
+  );
+}
+
+export default Main;
