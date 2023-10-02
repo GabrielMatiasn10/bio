4f2ddb85bc0588782b498c5f9bb562678bb8ef82
GabrielMatiasn10 <gm4037003@gmail.com>
2023-10-02 14:59:41 -0300

    add insta

--- src/assets/Footer.jsx
+++ src/assets/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
   return (
     <div>
     <footer>
-    <a href="https://www.instagram.com/gabrielmatiasn10/" target="_blank"> <BsInstagram/> </a>
+    <a href="https://www.instagram.com/gabriel_matiias7/" target="_blank"> <BsInstagram/> </a>
     <a href="Https://wa.me/5581989824985" target="_blank"> <BsWhatsapp/> </a>
     <a href="https://www.linkedin.com/in/gabriel-matias-dev/" target="_blank"> <BiLogoLinkedin/> </a>
     
