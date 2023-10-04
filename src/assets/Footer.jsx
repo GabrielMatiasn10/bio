94d6e37a8d7fa2bfaa0c843faa7889f281c935ca
GabrielMatiasn10 <gm4037003@gmail.com>
2023-10-03 23:57:16 -0300

    ajuste de linha

--- src/assets/Footer.jsx
+++ src/assets/Footer.jsx
@@ -10,7 +10,6 @@ function Footer() {
     <a href="https://www.instagram.com/gabriel_matiias7/" target="_blank"> <BsInstagram/> </a>
     <a href="Https://wa.me/5581989824985" target="_blank"> <BsWhatsapp/> </a>
     <a href="https://www.linkedin.com/in/gabriel-matias-dev/" target="_blank"> <BiLogoLinkedin/> </a>
-    
     </footer>
     <h5>
         @2023 Gabriel Matias
