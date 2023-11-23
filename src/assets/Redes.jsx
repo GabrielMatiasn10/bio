94b9ad70b3b59f9ba925dbd1f61ee213938fa5c5
GabrielMatiasn10 <gm4037003@gmail.com>
2023-11-22 23:21:44 -0300

    config button

--- src/assets/Redes.jsx
+++ src/assets/Redes.jsx
@@ -5,11 +5,10 @@ import {BiLogoLinkedin} from "react-icons/bi";
 function Redes() {
   return (
     <><div className="Redes">
-        <a href="https://www.instagram.com/gabriel_matiias7/" target="_blank"> <BsInstagram /> </a>
-        <a href="Https://wa.me/5581989824985" target="_blank"> <BsWhatsapp /> </a>
-        <a href="https://www.linkedin.com/in/gabriel-matias-dev/" target="_blank"> <BiLogoLinkedin /> </a>
+        <a href="https://www.instagram.com/gabriel_matiias7/" target="_blank"> <BsInstagram/> </a>
+        <a href="Https://wa.me/5581989824985" target="_blank"><BsWhatsapp /> </a>
+        <a href="https://www.linkedin.com/in/gabriel-matias-dev/" target="_blank"> <BiLogoLinkedin/> </a>
     </div>
-   
     </>
   );
 }
